17abce6755489bad10f6b1f33034ecabdba3fef1
SAM <samanthamorales8267@gmail.com>
2017-02-09 16:31:54 -0800

    Initial Commit

---
+++ Literals.js
@@ -0,0 +1,28 @@
+//Lab06 - Literals
+
+//Create object store and property/values
+var pikePlace ={
+  location: "Pike Place Market",
+  min:17,
+  max:88,
+  avg:5.2
+
+  //Generate random numbers with max/min range
+  getRandomCookie:  function(){
+    return Math.floor(((Math.random() * (88-17+ 1)) + 17)*5.2);
+  }
+}
+
+for (var i =0; i<hours.length; i++{
+  pikePlaceRandoms.push(pikePlace.getRandomCookie());
+}
+
+//Calculate sum
+var sum =0;
+for (var i=0; i<arr.length; i++){
+  sum = sum + arr[i];
+
+}
+return sum:
+}
+var pikePlaceTotal = arraySum(pikePlaceRandoms);
