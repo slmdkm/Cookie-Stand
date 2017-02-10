0ea15379126f53613530e1e0064484efd29004be
SAM <samanthamorales8267@gmail.com>
2017-02-09 20:38:01 -0800

    Java code is working, put not printing to HTML page

--- Literals.js
+++ Literals.js
@@ -5,24 +5,27 @@ var pikePlace ={
   location: "Pike Place Market",
   min:17,
   max:88,
-  avg:5.2
+  avg:5.2,
+  hours:["10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM","5PM"],
+  pikePlaceRandoms:[],
+
 
   //Generate random numbers with max/min range
-  getRandomCookie:  function(){
-    return Math.floor(((Math.random() * (88-17+ 1)) + 17)*5.2);
+  getRandomCookieCount:  function(){
+    return Math.floor(((Math.random() * (pikePlace.max-pikePlace.min) + pikePlace.min + 1)) + pikePlace.min * pikePlace.avg);
   }
-}
+};
 
-for (var i =0; i<hours.length; i++{
-  pikePlaceRandoms.push(pikePlace.getRandomCookie());
+for (var i =0; i<pikePlace.hours.length; i++){
+  pikePlace.pikePlaceRandoms.push(pikePlace.getRandomCookieCount());
 }
 
 //Calculate sum
 var sum =0;
-for (var i=0; i<arr.length; i++){
-  sum = sum + arr[i];
+for (var i=0; i<pikePlace.pikePlaceRandoms.length; i++){
+  sum = sum + pikePlace.pikePlaceRandoms[i];
 
 }
-return sum:
-}
-var pikePlaceTotal = arraySum(pikePlaceRandoms);
+ // return sum;
+
+var pikePlaceTotal = sum;
