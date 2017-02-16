7f4639b4feb5c1806a6a8a181231779aafbd6ab5
SAM <samanthamorales8267@gmail.com>
2017-02-16 09:31:50 -0800

    More modifications to JS

--- Constructor.js
+++ Constructor.js
@@ -1,104 +1,116 @@
-
 //Lab07-Constructors
-// function cookieStoreConstructor(location, min, max, avg, hours, randoms) {
-//   //var newCookieStore = {};
-//   newCookieStore.location = location;
-//   newCookieStore.min = min;
-//   newCookieStore.max = max;
-//   newCookieStore.avg = avg;
-//   newCookieStore.randoms = randoms;
-//   return newCookieStore;
-// }
-function cookieStoreConstructor(location, min, max, avg, hours, randoms) {
-  //var this = {};
+
+var hours = ["10AM", "11AM","12AM","1PM", "2PM", "3PM","4PM","5PM"];
+
+var table = document.getElementById("stores");
+var stores =[];
+
+var pikePlace = stores.push(new CookieStoreConstructor('Pike Place', 17, 88, 5.2,'pikeRow'));
+
+var seaTacAirport = stores.push(new CookieStoreConstructor('Sea Tac Airport', 6, 24, 1.2, 'seaTacRow'));
+
+var southcenter = stores.push(new CookieStoreConstructor('Southcenter',11, 38, 1.9, 'southcenterRow'));
+
+var bellevueSuare = stores.push (new CookieStoreConstructor('Bellevue Square',20, 48, 3.3,'bellevueRow'));
+
+var alki = stores.push(new CookieStoreConstructor('Alki', 3, 24, 2.6,'alkiRow'));
+
+
+
+function CookieStoreConstructor(location, min, max, avg, hours, storeRow) {
   this.location = location;
   this.min = min;
   this.max = max;
-  this.avg = avg;
-  this.randoms = randoms;
-  this.getRandomCookieCount= function(){
-    return Math.floor(((Math.random() * (this.max - this.min) + this.min + 1)) * this.avg);
-  };
+  this.avg = avg;//cookie per customer
+  this.row = storeRow;
+  this.totalSales=0;
+  this.hourlySales=[];
+};
 
-  //Cookies per hour
-  this.randoms = function(){
-    for (var i =0; i<pikePlace.hours.length; i++){
-      pikePlace.Randoms.push(pikePlace.getRandomCookieCount());
-
-      //Multiplied by average Cookies
-      console.log(pikePlace.Randoms[i]);
-    }
-  };
-  //Calculate sum - Randoms
-  var sum=0;
-  this.randoms = function(Randoms){
-    for (var i=0; i<this.randoms.length; i++){
-      sum = sum + this.randoms[i];
-      console.log('randoms: '+ this.randoms[i]);
-      console.log('sum:'+ sum);
-    }
-    return sum;
-
-  };
-  this.randoms.push(sum);
-
-  var store1 = cookieStoreConstructor('Pike Place', 17, 88, 5.2,[]);
-
-  var store2 = cookieStoreConstructor('Sea Tac Airport', 6, 24, 1.2, []);
-
-  var store3 = cookieStoreConstructor('Southcenter',11, 38, 1.9, []);
-
-  var store4 = cookieStoreConstructor('Bellevue Square',20, 48, 3.3,[]);
-
-  var store5 = cookieStoreConstructor('Alki', 3, 24, 2.6,[]);
-
-
-  for (i =0; i<8; i++){
-    //Create a cell
-    var cell = document.createElement('td');
-    //Create a text node, append the text to cell
-    cell.appendChild(document.createTextNode(cookieStore.hours[i] + ":"));
-    cell.appendChild(document.createTextNode(cookieStore.Randoms[i]));
-    //Append cell to table
-    parent.appendChild(cell);
+//Generates random number of Customers btw min and max per hour
+// Prototype property allows you to add a new property to an existing prototype which is CookieStoreConstructor.(So the property is: getRandomCustomer equalingthe function. )
+CookieStoreConstructor.prototype.getRandomCustomer = function(){
+  return Math.random()* (this.max - this.min) + this.min;
+};
+//Cookies per hour function
+//Prototype is: getHourlySales equaling the function.
+CookieStoreConstructor.prototype.getHourlySales = function (){
+  for (var i =0; i< hours.length; i++){
+    var rand = Math.floor(this.getRandomCustomer() * this.avg);
+    this.hourlySales.push(rand);
+    this.totalSales += rand;
   }
-  child=document.createElement('td');
-  child.textContent = 'Total: '+ cookieStoreSum();
-  parent.appendChild(child);
-}
+};
+//Prototype is: render equaling the function.
+  CookieStoreConstructor.prototype.render = function(){
+    var storeSection = document.getElementById('stores');
+    var row = document.createElement('tr');//var row - make a new table row
+    var td = document.createElement('td');//var td - make a new table cell
+    td.innerHTML = this.location;
+    row.appendChild(td);
 
-    this.render = function(){
-      var storeList = document.getElementById('storeList');
-      var liElement = document.createElement('li');
-      liElement.textContent = this.location + '' +this.min + '' + this.max + ''+ this.avg + '' + this.randoms;
-      storeList.appendChild(liElement);
+//innerHTML -adding or removing content from DOM Tree (2nd is Dom Manipulation)
 
+    for (var i =0; i< this.hourlySales.length; i++){
+      var tdHourly = document.createElement('td');//Create element td node, store in a var
+      tdHourly.innerHTML = this.hourlySales[i];
+      row.appendChild(tdHourly);//Append tdHourly to the row.
     }
+  var total = document.createElement('td');
+  total.innerHTML = this.totalSales
+  row.appendChild(total);
+  storeSection.appendChild(row);
+};
 
-    this.renderAsRow = function(){
-      var storeTable = document.getElementById('storeTable');
-      var trElement = document.createElement('tr');
-      var locationData=document.createElement('td');
-      var minData = document.createElement('td');
-      var maxData =document.createElement('td');
-      var avgData =document.createElement('td');
-      var randomsData =document.createElement('td');
-      locationData.textContent=this.location;
-      minData.textContent = this.min;
-      maxData.textContent = this.max;
-      avgData.textContent = this.avg;
-      randomsData.textContent = this.randoms;
-      trElement.appendChild(locationData);
-      trElement.appendChild(minData);
-      trElement.appendChild(maxData);
-      trElement.appendChild(avgData);
-      trElement.appendChild(randomsData);
-      storeTable.appendChild(trElement);
+function generateTableHeading(){
+  var storeSection = document.getElementById('stores');
+  var thead = document.createElement('thead');
+  storeSection.appendChild(thead);
+  var row = document.createElement('tr');
+  thead.appendChild(row);
+  var td = document.createElement('td');
+  td.innerHTML = "Location";
+  row.appendChild(td);
 
+  for (var i = 0; i < hours.length; i++) {
+    var td = document.createElement('td');
+    td.innerHTML = hours[i];
+    row.appendChild(td);
+  }
+
+  var tdTotal = document.createElement('th');
+  tdTotal.textContent = this.totalSales;
+  tdTotal.innerHTML = "Total";
+  row.appendChild(tdTotal);
+  storeSection.appendChild(row);
 }
 
-    store1.renderAsRow();
-    store2.renderAsRow();
-    store3.renderAsRow();
-    store4.renderAsRow();
-    store5.renderAsRow();
+generateTableHeading();
+stores.forEach(function(store){
+  store.getHourlySales();
+  store.render();
+});
+//Event triggers JS code - event target is topmost target in Dom tree
+//Reset values back to 0.
+var clearFields = function(event){
+  event.target.storeLocal.value = null;
+  event.target.minInput.value = null;
+  event.target.maxInput.value = null;
+  event.target.avgInput.value = null;
+};
+
+var formEl = document.getElementById("input Form");
+formEl.addEventListener("submit", function(event) {
+  event.preventDefault();
+  var newStoreName = (event.target.storeLocal.value);
+  var newMin = parseInt(event.target.minInput.value);
+  var newMax = parseInt(event.target.maxInput.value);
+  var newAvg = parseFloat(event.target.avgInput.value);
+
+  var newRow = newStoreName + "Row";
+  var newStore = new CookieStoreConstructor(newStoreName, 8, newMin, newMax, newAvg, newRow);
+  newStore.getHourlySales();
+  newStore.render();
+  stores.push(newStore);
+  clearFields(event);
+});
